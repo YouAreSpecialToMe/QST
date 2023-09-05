33503b983a37cf92af3a12f6bac61369fba8a4fb
Pontus <39851333+YouAreSpecialToMe@users.noreply.github.com>
2023-09-05 10:04:36 -0400

    Add files via upload

--- trainer_qa.py
+++ trainer_qa.py
@@ -3,33 +3,41 @@ import pickle
 task = "cola"
 
 model_name = "opt-6.7b"
-with open(f"glue_lst_mrpc_{model_name}.pickle", "rb") as f:
+with open(f"glue_lst_{task}_{model_name}.pickle", "rb") as f:
     loaded_dict = pickle.load(f)
 # print(loaded_dict)
 
 values = []
 for elem in loaded_dict["log"]:
-    print(elem)
+    # print(elem)
     # if 'loss' in elem.keys():
     #     values.append(elem['loss'])"eval_matthews_correlation"
-    if "eval_accuracy" in elem.keys():
+    if task == "cola":
+        values.append(elem['eval_matthews_correlation'])
+    elif task == "stsb":
+        values.append(elem['eval_pearson'])
+    else:
         values.append(elem['eval_accuracy'])
 
 max_lora = max(values)
 print(max_lora)
 
 # model_name = "/home/zzx/pythonproject/LLM/huggingface-demos/experiments/faster_generation/opt-1.3b"
-# with open(f"glue_lora_stsb_{model_name}.pickle", "rb") as f:
-#     loaded_dict = pickle.load(f)
-# # print(loaded_dict)
-#
-# values = []
-# for elem in loaded_dict["log"]:
-#     # print(elem)
-#     # if 'loss' in elem.keys():
-#     #     values.append(elem['loss'])eval_accuracy
-#     if "eval_accuracy" in elem.keys():
-#         values.append(elem['eval_accuracy'])
-#
-# max_moelora = max(values)
-# print(max_moelora)
+with open(f"glue_lst_{task}_{model_name}.pickle", "rb") as f:
+    loaded_dict = pickle.load(f)
+# print(loaded_dict)
+
+values = []
+for elem in loaded_dict["log"]:
+    # print(elem)
+    # if 'loss' in elem.keys():
+    #     values.append(elem['loss'])eval_accuracy
+    if task == "cola":
+        values.append(elem['eval_matthews_correlation'])
+    elif task == "stsb":
+        values.append(elem['eval_pearson'])
+    else:
+        values.append(elem['eval_accuracy'])
+
+max_moelora = max(values)
+print(max_moelora)
