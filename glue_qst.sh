6869c5bbf9f9b43bc0833c3ddb65ad1d0b4adc52
zhengxin <39851333+YouAreSpecialToMe@users.noreply.github.com>
2023-09-15 09:51:28 -0400

    Create glue_qst.sh

---
+++ glue_qst.sh
@@ -0,0 +1,5 @@
+python glue_lstquant.py --batch_size 1
+python glue_lstquant.py --batch_size 2
+python glue_lstquant.py --batch_size 4
+python glue_lstquant.py --batch_size 8
+python glue_lstquant.py --batch_size 16
