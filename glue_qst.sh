b41fe83a8a46a7a53747b5dedcded987b38ef2ab
zhengxin <39851333+YouAreSpecialToMe@users.noreply.github.com>
2023-09-15 10:04:02 -0400

    Update glue_qst.sh

--- glue_qst.sh
+++ glue_qst.sh
@@ -1,5 +1,19 @@
-python glue_lstquant.py --batch_size 1
-python glue_lstquant.py --batch_size 2
-python glue_lstquant.py --batch_size 4
-python glue_lstquant.py --batch_size 8
-python glue_lstquant.py --batch_size 16
+python glue_lstquant.py --batch_size 1 --model_checkpoint opt-1.3b
+python glue_lstquant.py --batch_size 2 --model_checkpoint opt-1.3b
+python glue_lstquant.py --batch_size 4 --model_checkpoint opt-1.3b
+python glue_lstquant.py --batch_size 8 --model_checkpoint opt-1.3b
+python glue_lstquant.py --batch_size 16 --model_checkpoint opt-1.3b
+python glue_lstquant.py --batch_size 32 --model_checkpoint opt-1.3b
+
+# python glue_lstquant.py --batch_size 1 --model_checkpoint opt-2.7b
+# python glue_lstquant.py --batch_size 2 --model_checkpoint opt-2.7b
+# python glue_lstquant.py --batch_size 4 --model_checkpoint opt-2.7b
+# python glue_lstquant.py --batch_size 8 --model_checkpoint opt-2.7b
+# python glue_lstquant.py --batch_size 16 --model_checkpoint opt-2.7b
+
+# python glue_lstquant.py --batch_size 1 --model_checkpoint opt-6.7b
+# python glue_lstquant.py --batch_size 2 --model_checkpoint opt-6.7b
+# python glue_lstquant.py --batch_size 4 --model_checkpoint opt-6.7b
+# python glue_lstquant.py --batch_size 8 --model_checkpoint opt-6.7b
+# python glue_lstquant.py --batch_size 16 --model_checkpoint opt-6.7b
+# python glue_lstquant.py --batch_size 32 --model_checkpoint opt-6.7b
