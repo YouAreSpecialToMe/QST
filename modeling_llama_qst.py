b9542d5d5fa2403e6eb085aeee7e1bd091ea2693
zhengxin <39851333+YouAreSpecialToMe@users.noreply.github.com>
2024-10-17 21:11:31 -0400

    Update modeling_llama_qst.py

--- modeling_llama_qst.py
+++ modeling_llama_qst.py
@@ -39,7 +39,8 @@ from transformers.models.llama.configuration_llama import LlamaConfig
 
 from QSTGenerationMixin import QSTGenerationMixin
 from QSTConfig import AdapterLinear
-from modeling_qst_output import QSTBaseModelOutputWithPast, QSTCausalLMOutputWithPast
+from modeling_qst_output import QSTBaseModelOutputWithPast, QSTCausalLMOutputWithPast, \
+    QSTSequenceClassifierOutputWithPast
 
 logger = logging.get_logger(__name__)
 
@@ -1688,7 +1689,7 @@ class LlamaForSequenceClassification(LlamaPreTrainedModel):
         )
 
 
-class QSTLlamaForSequenceClassification(LlamaPreTrainedModel):
+class QSTLlamaForSequenceClassification(LlamaPreTrainedModel, QSTGenerationMixin):
     def __init__(self, llm: LlamaForSequenceClassification, config: LlamaConfig, qstconfig):
         super().__init__(config)
         self.num_labels = llm.num_labels
@@ -1735,11 +1736,14 @@ class QSTLlamaForSequenceClassification(LlamaPreTrainedModel):
             attention_mask: Optional[torch.Tensor] = None,
             position_ids: Optional[torch.LongTensor] = None,
             past_key_values: Optional[List[torch.FloatTensor]] = None,
+            qst_past_key_values: Optional[List[torch.FloatTensor]] = None,
             inputs_embeds: Optional[torch.FloatTensor] = None,
             labels: Optional[torch.LongTensor] = None,
             use_cache: Optional[bool] = None,
             output_attentions: Optional[bool] = None,
             output_hidden_states: Optional[bool] = None,
+            output_qst_attentions: Optional[bool] = None,
+            output_qst_hidden_states: Optional[bool] = None,
             return_dict: Optional[bool] = None,
     ) -> Union[Tuple, SequenceClassifierOutputWithPast]:
         r"""
@@ -1755,10 +1759,13 @@ class QSTLlamaForSequenceClassification(LlamaPreTrainedModel):
             attention_mask=attention_mask,
             position_ids=position_ids,
             past_key_values=past_key_values,
+            qst_past_key_values=qst_past_key_values,
             inputs_embeds=inputs_embeds,
             use_cache=use_cache,
             output_attentions=output_attentions,
+            output_qst_attentions=output_qst_attentions,
             output_hidden_states=output_hidden_states,
+            output_qst_hidden_states=output_qst_hidden_states,
             return_dict=return_dict,
         )
 
@@ -1819,12 +1826,15 @@ class QSTLlamaForSequenceClassification(LlamaPreTrainedModel):
             output = (pooled_logits,) + transformer_outputs[1:]
             return ((loss,) + output) if loss is not None else output
 
-        return SequenceClassifierOutputWithPast(
+        return QSTSequenceClassifierOutputWithPast(
             loss=loss,
             logits=pooled_logits,
             past_key_values=transformer_outputs.past_key_values,
+            qst_past_key_values=transformer_outputs.qst_past_key_values,
             hidden_states=transformer_outputs.hidden_states,
             attentions=transformer_outputs.attentions,
+            qst_hidden_states=transformer_outputs.qst_hidden_states,
+            qst_attentions=transformer_outputs.qst_attentions,
         )
 
     def load_qst_state(self, path):
